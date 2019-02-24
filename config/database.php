f2a445957fed7f28b11051f14b8ba89adfd62ae9
amcaristan <amcaristan@gmail.com>
2019-02-24 11:50:33 -0800

    ENV vars updated
    Environment variables updated to reflect AWS EB generated variables

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST') ?? $_SERVER['RDS_HOSTNAME'],
+            'port' => env('DB_PORT') ?? $_SERVER['RDS_PORT'],
+            'database' => env('DB_DATABASE') ?? $_SERVER['RDS_DB_NAME'],
+            'username' => env('DB_USERNAME') ?? $_SERVER['RDS_USERNAME'],
+            'password' => env('DB_PASSWORD') ?? $_SERVER['RDS_PASSWORD'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
